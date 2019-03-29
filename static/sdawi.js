c641a5bc83db9343b4d7dd4716c9fb9ff96c85e9
Alexandr <stefanitsky.mozdor@gmail.com>
2019-03-29 18:01:53 +0300

    Design improves
    Added: full height page, fixed left column (with tree)
    Summary: better css design, fixed sql tab content showing (hides sql result before request)

--- static/sdawi.js
+++ static/sdawi.js
@@ -118,7 +118,7 @@ $('.db_tree').on('select_node.jstree', function(e, data) {
     data.instance.open_node(data.node);
 });
 // Add events on click of the submit query button
-$('#submit_query').click(function(event) {
+$('#submit_query_button').click(function(event) {
     query = sql_input_area.getDoc().getValue();
     raw_sql_request.update_query(query);
     raw_sql_request.request();
