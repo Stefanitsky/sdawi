b702b6e8f0947b1341a83fcdcf96bbf9047e05d4
Alexandr <stefanitsky.mozdor@gmail.com>
2019-03-29 13:38:18 +0300

    Icon links changed
    The icons were downloaded and placed in a local directory so that they are stored on the server.

--- sdawi.py
+++ sdawi.py
@@ -120,7 +120,7 @@ def build_db_tree(db_names, request_tables_list_for_db):
         'id': g.db_host,
         'parent': '#',
         'text': g.db_host,
-        'icon': 'https://img.icons8.com/dusk/24/000000/server.png',
+        'icon': '/static/icons/host.png',
         'a_attr': {
             'type': 'host'
         }
@@ -131,7 +131,7 @@ def build_db_tree(db_names, request_tables_list_for_db):
             'id': db_name,
             'parent': g.db_host,
             'text': db_name,
-            'icon': 'https://img.icons8.com/dusk/24/000000/database.png',
+            'icon': '/static/icons/database.png',
             'a_attr': {
                 'type': 'db'
             }
@@ -144,8 +144,7 @@ def build_db_tree(db_names, request_tables_list_for_db):
                     'id': table[0],
                     'parent': db_name,
                     'text': table[0],
-                    'icon':
-                    'https://img.icons8.com/dusk/24/000000/grid.png',
+                    'icon': '/static/icons/table.png',
                     'a_attr': {
                         'type': 'table'
                     }
