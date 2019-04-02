b5fcd8207355118717a556fc38f4a0d04d010400
Alexandr <stefanitsky.mozdor@gmail.com>
2019-04-02 12:50:53 +0300

    Front-end code comments
    Added: comments for the front-end code (for own files, not for dependencies)

--- static/sdawi/tabs.js
+++ static/sdawi/tabs.js
@@ -1,14 +1,23 @@
+/*
+* Global values for each tab
+*/
 var welcome_tab = $($("#tabs ul").find("li")[0]);
 var structure_tab = $($("#tabs ul").find("li")[1]);
 var table_data_tab = $($("#tabs ul").find("li")[2]);
 var sql_tab = $($("#tabs ul").find("li")[3]);
 
+/*
+* Functions for managing tabs.
+*/
+
+// Returns the Raw SQL tab to its original state
 function load_sql_tab() {
     $('#error_sql_result').hide();
     $('#success_sql_result').hide();
     sql_tab.show();
 }
 
+// Hides unnecessary tabs on the main page when the user is logged in
 function main_tabs_show() {
     welcome_tab.show();
     structure_tab.hide();
@@ -17,6 +26,7 @@ function main_tabs_show() {
     tabs.open(0);
 }
 
+// Hides unnecessary tabs when selecting a database
 function database_tabs_show() {
     welcome_tab.hide();
     structure_tab.show();
@@ -25,6 +35,7 @@ function database_tabs_show() {
     tabs.open(1);
 }
 
+// Hides unnecessary tabs when selecting a table
 function table_tabs_show() {
     welcome_tab.hide();
     structure_tab.show();
@@ -33,7 +44,7 @@ function table_tabs_show() {
     tabs.open(1);
 }
 
+// Returns the Raw SQL tab to its original state when it was clicked
 sql_tab.click(function(event) {
     load_sql_tab();
 });
-
